c8b386ea2ad996de4dc7350b536b6fe324f4b7f1
Nikita Ilin <ilinWall@yandex.ru>
2022-06-28 01:37:40 +0300

    Fix isort.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import serializers
-
 from reviews.models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
